ac4d5385c249b155e4b161396f8cf03839b7759b
Jim Patt & David Garber <student@epicodus.com>
2015-05-12 16:42:05 -0700

    unfinished front end

--- app.rb
+++ app.rb
@@ -18,38 +18,47 @@ end
 
 post('/add_survey/') do
   Survey.create(name: params.fetch("name"))
-  @surveys = Survey.all()
-  @question = @surveys[0]
   erb(:add_question)
 end
 
 get('/take_survey/') do
   @surveys = Survey.all()
-  @questions = Question.all()
-  @question = @questions[0]
+  @question = Question.find()
   erb(:take_survey)
 end
 
-get('/add_question/') do
+get('/surveys/:id') do
+  @survey = Survey.find(params.fetch('id'))
+  erb(:survey)
+end
+
+get('/surveys/:id/add_question') do
   erb(:add_question)
 end
 
-post('/add_question/') do
+post('/surveys/:id/add_question') do
+  @surveys = Survey.all()
+  survey_id = params.fetch('id')
+
   question = params.fetch('question')
   answer1 =params.fetch('answer1')
   answer2 =params.fetch('answer2')
   answer3 =params.fetch('answer3')
   answer4 =params.fetch('answer4')
   answer5 =params.fetch('answer5')
-  Question.create({:question => question, :answer1 => answer1, :answer2 => answer2, :answer3 => answer3, :answer4 => answer4, :answer5 => answer5 })
+  Question.create({:survey_id => survey_id, :question => question, :answer1 => answer1, :answer2 => answer2, :answer3 => answer3, :answer4 => answer4, :answer5 => answer5 })
   erb(:add_question)
 end
 
+# get('/surveys/:survey_id/questions/:id') do
+#   survey = Survey.find(params.fetch('survey_id'))
+#   question = Question.find(params.fetch('id'))
+#
+# end
 
 get('/question/:id') do
   @questions = Question.all()
-binding.pry
-  @question = Question.find("id".to_i())
+  @question = Question.find(params.fetch("id").to_i())
   erb(:question)
 end
 
